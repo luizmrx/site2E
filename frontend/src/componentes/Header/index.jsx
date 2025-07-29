a0aad6275cfe7ec3fee14d1f3c080e3b0bc2045e
Luiz Souza <luizmrx91@gmail.com>
2025-07-29 13:09:10 -0300

    feat: atualizações no header para ter efeitos de sombra, menu suspenso mais interativo, e melhor design

--- frontend/src/componentes/Header/index.jsx
+++ frontend/src/componentes/Header/index.jsx
@@ -1,28 +1,57 @@
 import './Header.css'
 import Botao from '../Botao'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Header = () => {
+
+    const [aberto, setAberto] = useState(false);
+    const alternarMenu = () => {
+        setAberto((atual) => !atual);
+    };
+
     return (
         <header>
             <nav className='cabecalho'>
-                <Link className='' to={"/"}>
-                    <img src="src/assets/logo__branco.svg" alt="Logo da 2E" />
-                </Link>
                 <div className='cabecalho__opcoes'>
-                    <Botao texto="Contato"/>
-                    <input type="checkbox" id='menu' className='check'/>
-                    <label htmlFor="menu" className='menu__hamburguer'>
+                    <button onClick={alternarMenu} className='cabecalho__botao'>
                         <img src="src/assets/menu__hamburguer.svg" alt="Menu hamburguer" />
-                    </label>
-                    <ul className='cabecalho__links'>
-                        <li className='cabecalho__item'>
-                            <Link className='cabecalho__link' to={"/sobre"}>Sobre</Link>
-                        </li>
-                        <li className='cabecalho__item'>
-                            <Link className='cabecalho__link' to={"/seguranca"}>Segurança</Link>
-                        </li>
-                    </ul>
+                    </button>
+                    {aberto && (
+                        <>
+                            <div className="overlay" onClick={alternarMenu}></div>
+                            <ul className='cabecalho__links'>
+                                <li className=''>
+                                    <button onClick={alternarMenu} className='cabecalho__botao cabecalho__link'
+                                    ><img src="src/assets/menu__hamburguer.svg" alt="Menu hamburguer" /></button>
+                                </li>
+                                <li className=''>
+                                    <Link className='cabecalho__link' to={"/"}>
+                                        <button onClick={alternarMenu} className='cabecalho__botao__suspenso cabecalho__link'
+                                        >Inicio</button>
+                                    </Link>
+                                </li>
+                                <li className=''>
+                                    <Link className='cabecalho__link' to={"/sobre"}>
+                                        <button onClick={alternarMenu} className='cabecalho__botao__suspenso cabecalho__link'
+                                        >Sobre</button>
+                                    </Link>
+                                </li>
+                                <li className=''>
+                                    <Link className='cabecalho__link' to={"/seguranca"}>
+                                        <button onClick={alternarMenu} className='cabecalho__botao__suspenso cabecalho__link'
+                                        >Segurança</button>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </>
+                    )}
+                    <Link className='' to={"/"}>
+                        <img src="src/assets/logo__branco.svg" alt="Logo da 2E" />
+                    </Link>
+                </div>
+                <div className='cabecalho__botao'>
+                    <Botao texto="Contato" />
                 </div>
             </nav>
         </header>
